263f5565aaf109d3114947728baba039e481f4a4
supunwi <supunw@embla.asia>
2025-07-14 15:13:48 +0530

    Removed apikeys from script files

--- demo/html/script.js
+++ demo/html/script.js
@@ -7,13 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     height: '58px',
     transcription: {
       provider: 'default',
-      // apiKey: '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'
-      apiKey: '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'
+      apiKey: ''
     },
     recognition: {
       provider: 'openai',
-      // apiKey: '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'
-      apiKey: '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'
+      apiKey: ''
     },
     ui: {
       url : 'custom-style.css',  
