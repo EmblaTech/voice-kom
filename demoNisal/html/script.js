9f13ea4b0067b5bfe1c67e721a5a753007bf87c5
supunwi <supunw@embla.asia>
2025-07-14 15:33:49 +0530

    Removed keys

--- demoNisal/html/script.js
+++ demoNisal/html/script.js
@@ -80,17 +80,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // containerId: 'voice-lib-container', // Changed from container element to containerId string
     // lang: 'no',
     // sttEngine: 'default',
-    // sttApiKey: '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',
+    // sttApiKey: '',
     // nluEngine: 'llm',
-    // nluApiKey: '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',
+    // nluApiKey: '',
   
     transcription: {
       provider: 'default',
-      apiKey: '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'
+      apiKey: ''
     },
     recognition: {
       provider: 'openai',
-      apiKey: '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'
+      apiKey: ''
     },
 
     clientId: 'a1b2c3d4-e5f6-7890-1234-567890abcdef',
